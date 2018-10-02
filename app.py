45c2e36eae0b0a944f3d31399d50143777d0aa6e
IOLevi <337@holbertonschool.com>
2018-10-02 13:41:53 -0700

    reorganization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 "App module"
-from flask import Flask, make_response, jsonify, Response, abort, render_template
+from flask import Flask, make_response, jsonify, Response, abort, render_template, request
 import os
 from flask_cors import CORS
 from dbs import storage
@@ -25,39 +25,34 @@ def not_found(error):
 def display_splash():
     """prints splash of things"""
 
-    # heroes = list(storage.all().values())
-    # supports = [healer for healer in heroes if healer.heroClass == 'Support']
-    # warriors = [warrior for warrior in heroes if healer.heroClass == 'Warrior']
-    # assassins = [assassin for assassin in heroes if healer.heroClass == 'Assassin']
-# add .desc() to the end to sort em up
-    # supports = storage.session.query(HeroTemplate).filter_by(heroClass='Support').filter(HeroTemplate.gamesPlayed > 1000).order_by(HeroTemplate.winRate.desc())[0:3]
     supports = storage.session.query(HeroTemplate).filter_by(heroClass='Support').order_by(HeroTemplate.winRate.desc())
     warriors = storage.session.query(HeroTemplate).filter_by(heroClass='Warrior').order_by(HeroTemplate.winRate.desc())
-    # warriors = storage.session.query(HeroTemplate).filter_by(heroClass='Warrior').filter(HeroTemplate.gamesPlayed > 1000).order_by(HeroTemplate.winRate.desc())[0:3]
     assassins = storage.session.query(HeroTemplate).filter_by(heroClass='Assassin').order_by(HeroTemplate.winRate.desc())
     # assassins = storage.session.query(HeroTemplate).filter_by(heroClass='Assassin').filter(HeroTemplate.gamesPlayed > 1000).order_by(HeroTemplate.winRate.desc())[0:3]
 
     return render_template("index.html", warriors=warriors, supports=supports, assassins=assassins)
 
-# @app.route('/<string:shorturl>', strict_slashes=False, methods=['GET'])
-# def testshort2(shorturl):
-#     #look up in db
-#     target = storage.get(shorturl)
-#     if target:
-#         return target.actualurl
-#     else:
-#         return "BORKED"
+@app.route('/api/v1/supports', strict_slashes=False, methods=['GET'])
+def get_supports():
+    supports = storage.session.query(HeroTemplate).filter_by(heroClass='Support').order_by(HeroTemplate.winRate.desc())
 
-# @app.route('/short/<string:url>', strict_slashes=False, methods=['GET'])
-# def testshort(url):
-#     #make a short url hash that will be the short url
-#     #havent dealt with collisions yet
-#     short = urlshort(url)
-#     #add to db
-#     att = {'urlhash':short, 'actualurl':url}
-#     a = ShortURL(**att)
-#     a.save()
-#     return "www.ourweb.com/" + short
+    return jsonify([sup.to_dict() for sup in supports])
+@app.route('/api/v1/warriors', strict_slashes=False, methods=['GET'])
+def get_warriors():
+    warriors = storage.session.query(HeroTemplate).filter_by(heroClass='Warrior').order_by(HeroTemplate.winRate.desc())
+
+    return jsonify([war.to_dict() for war in warriors])
+
+@app.route('/api/v1/assassins', strict_slashes=False, methods=['GET'])
+def get_assassins():
+    assassins = storage.session.query(HeroTemplate).filter_by(heroClass='Assassin').order_by(HeroTemplate.winRate.desc())
+
+    return jsonify([ass.to_dict() for ass in assassins])
+
+@app.route('/api/dev', strict_slashes=False)
+def get_dev_page():
+    return render_template("/api/dev.html")
+
 if __name__ == "__main__":
     app.run(
             host=os.getenv("HBNB_API_HOST") if os.getenv("HBNB_API_HOST")
